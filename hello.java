6c2d46a5be446f7e88c45b6cbc61ba9723362ac1
JitenkumarBehera56 <jitenkumarbehera71@gmail.com>
2024-11-09 11:26:51 +0530

    1st java

---
+++ hello.java
@@ -0,0 +1,11 @@
+public class hello 
+{
+    
+    public static void main(String[] args) 
+    {
+        int a=2,b=5;
+        int sum=a+b;
+        int mul=a*b;
+        System.out.println(+sum+" , "+mul);
+    }
+}
