0021c3e0f6d41f01da330aac7027a4c42a71ede7
Jiten kumar Behera <jitenkumarbehera71@gmail.com>
2024-12-16 14:44:05 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -6,6 +6,6 @@ public class hello
         int a=2,b=5;
         int sum=a+b;
         int mul=a*b;
-        System.out.println(+sum+" , "+mul);
+        System.out.println(sum+" , "+mul);
     }
 }
