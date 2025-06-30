ab69cc351d58899aa8e3e14c8f75183daafde973
Jiten kumar Behera <jitenkumarbehera71@gmail.com>
2025-06-30 23:13:37 +0530

    Update Event1.java

--- Event1.java
+++ Event1.java
@@ -1,38 +1,3 @@
-// import java.awt.Button;
-// import java.awt.Frame;
-// import java.awt.TextField;
-// import java.awt.event.ActionEvent;
-// import java.awt.event.ActionListener;
-// class Event1 extends Frame implements ActionListener
-// {
-//     TextField f;
-//     Event1()
-//     {
-//         f=new TextField();
-//         f.setBounds(60,50,170,20);
-//         Button b=new Button("NOTICE");
-//         b.setBounds(100,20,80,80);
-//         b.addActionListener(this);;
-//         add(b);
-//         add(f);
-//         setSize(300,300);
-//         setLayout(null);
-//         setVisible(true);
-//     }
-//     public void ActionPerformed(ActionEvent e)
-//     {
-//         f.setText("WELCOME");
-//     }
-//     public static void main(String[] args) 
-//     {
-//         new Event1();
-//     }
-
-//     @Override
-//     public void actionPerformed(ActionEvent e) {
-//         throw new UnsupportedOperationException("Not supported yet.");
-//     }
-// }
 
 import java.awt.Button;
 import java.awt.Frame;
