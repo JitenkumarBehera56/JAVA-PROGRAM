e7cb7ddf1f35227684f34605c10576d269e7567b
Jiten kumar Behera <jitenkumarbehera71@gmail.com>
2025-06-30 23:20:06 +0530

    Add files via upload

---
+++ Testsynchronised.java
@@ -0,0 +1,56 @@
+class Table
+{
+    synchronized void print(int n)                                
+    {
+         for(int i=1;i<=5;i++)
+        {
+            System.out.println(n*i);
+            try
+            {
+                Thread.sleep(400);    
+            }
+            catch(Exception e)
+            {
+                System.out.println("e");
+            }
+        }
+    }
+}
+
+class myThread1 extends Thread
+{
+    Table t;
+    myThread1(Table t)
+    {
+        this.t=t;
+    }
+    public void run()
+    {
+        t.print(5);
+    }
+}
+
+class myThread2 extends Thread
+{
+    Table t;
+    myThread2(Table t)
+    {
+        this.t=t;
+    }
+    public void run()
+    {
+        t.print(100);
+    }
+}
+
+class Testsynchronised
+{
+    public static void main(String[] args) 
+    {
+    Table o=new Table();
+    myThread1 t1=new myThread1(o);
+    t1.start();    
+    myThread2 t2=new myThread2(o);
+    t2.start();
+    }
+}
